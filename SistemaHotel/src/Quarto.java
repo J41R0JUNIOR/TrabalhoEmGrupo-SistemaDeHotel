eaf8760eeb9c48b8b6b0dc2b019c6f27e3f967c4
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-05-01 20:46:07 -0300

    Terminando de criar a classe quarto, agora com um array de hospedes, um bool pra saber se o quarto foi limpo

--- SistemaHotel/src/Quarto.java
+++ SistemaHotel/src/Quarto.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -5,14 +6,49 @@ public class Quarto {
     private final int numero;
     private final int capacidadeMaxima = 4;
     private int ocupacaoAtual;
-    private boolean chaveNaRecepcao;
+    private List<Hospede> hospedes;
+    private boolean limpo;
     private final Lock lock;
 
-    public Quarto(int numero) {
+    public Quarto(int numero, List<Hospede> hospedes) {
         this.numero = numero;
         this.ocupacaoAtual = 0;
-        this.chaveNaRecepcao = true;
+        this.hospedes = hospedes;
+        this.limpo = false;
+
         this.lock = new ReentrantLock();
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getCapacidadeMaxima() {
+        return capacidadeMaxima;
+    }
+
+    public int getOcupacaoAtual() {
+        return ocupacaoAtual;
+    }
+
+    public void setOcupacaoAtual(int ocupacaoAtual) {
+        this.ocupacaoAtual = ocupacaoAtual;
+    }
+
+    public List<Hospede> getHospedes() {
+        return hospedes;
+    }
+
+    public void setHospedes(List<Hospede> hospedes) {
+        this.hospedes = hospedes;
+    }
+
+    public boolean isLimpo() {
+        return limpo;
+    }
+
+    public void setLimpo(boolean limpo) {
+        this.limpo = limpo;
     }
 }
 
