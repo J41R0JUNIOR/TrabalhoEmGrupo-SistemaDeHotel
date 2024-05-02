ddbef42baafbee41955655206d6ba589d270f8f8
Guilherme Akio <87677104+guiakio8@users.noreply.github.com>
2024-05-01 22:22:36 -0300

    Criado classe Hospede

--- SistemaHotel/src/Hospede.java
+++ SistemaHotel/src/Hospede.java
@@ -1,12 +1,24 @@
 public class Hospede extends Thread{
+
     private Integer id;
+    //    private boolean chave = false;
+    private Integer numeroQuarto;
 
     public Hospede(Integer id){
         this.id = id;
+        this.numeroQuarto = numeroQuarto;
     }
 
-    public void run(){
-        
+    public void setNumeroQuarto(Integer numeroQuarto) {
+        this.numeroQuarto = numeroQuarto;
     }
 
+    public Integer getNumeroQuarto() {
+        return numeroQuarto;
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
 }
