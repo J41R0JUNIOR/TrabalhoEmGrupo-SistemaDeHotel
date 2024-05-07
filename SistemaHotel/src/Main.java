cec87a3905838280ee0cce7a8c6343728d59907d
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-05-06 21:41:59 -0300

    retirando grupos com zero membros

--- SistemaHotel/src/Main.java
+++ SistemaHotel/src/Main.java
@@ -19,31 +19,35 @@ public class Main {
             ArrayList<Hospede> hospedesGrupo = new ArrayList<>();
 
             // Gera uma quantidade aleatória de hóspedes para o grupo atual
-            int tamanhoGrupo = rand.nextInt(hospedes.size()) + 1;
+            if (hospedes.size() > 0) {
+                int tamanhoGrupo = rand.nextInt(hospedes.size()) + 1;
 
-            if (tamanhoGrupo > 10){
-                tamanhoGrupo = rand.nextInt(hospedes.size()/2 + 1);
+
+//            if (tamanhoGrupo > 10){
+//                tamanhoGrupo = rand.nextInt(hospedes.size()/2 + 1);
+//            }
+
+                // Adiciona hóspedes ao grupo
+                for (int i = 0; i < tamanhoGrupo; i++) {
+                    hospedes.get(0).groupId = indexGroupId;
+                    hospedesGrupo.add(hospedes.remove(0)); // Remove e adiciona o primeiro hóspede da lista
+                    //adicioar quarto para cada hospede
+                }
+
+                GrupoHospedes novoGrupo = new GrupoHospedes(hospedesGrupo, indexGroupId);
+                grupos.add(novoGrupo);
+                System.out.println("Grupo " + novoGrupo.idGrupo + " criado, " + tamanhoGrupo + " membros: ");
+
+                // Printando todos os membros de todos os grupos
+                for (Hospede hospede : novoGrupo.getParticipantes()) {
+                    System.out.print("- " + hospede.getName());
+                }
+
+                System.out.println();
+
+                // Index geral do grupo
+                indexGroupId += 1;
             }
-
-            // Adiciona hóspedes ao grupo
-            for (int i = 0; i < tamanhoGrupo; i++) {
-                hospedes.get(0).groupId = indexGroupId;
-                hospedesGrupo.add(hospedes.remove(0)); // Remove e adiciona o primeiro hóspede da lista
-            }
-
-            GrupoHospedes novoGrupo = new GrupoHospedes(hospedesGrupo, indexGroupId);
-            grupos.add(novoGrupo);
-            System.out.println("Grupo " + novoGrupo.idGrupo + " criado, " + tamanhoGrupo + " membros: ");
-
-            // Printando todos os membros de todos os grupos
-            for (Hospede hospede : novoGrupo.getParticipantes()) {
-                System.out.print("- " + hospede.getName());
-            }
-
-            System.out.println();
-
-            // Index geral do grupo
-            indexGroupId += 1;
         }
 
 
