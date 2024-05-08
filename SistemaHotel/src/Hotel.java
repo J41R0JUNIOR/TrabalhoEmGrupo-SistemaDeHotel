e9d4a87f4a1a475d74c5321690da75aab95fb86c
Guilherme Akio <guiakio8@gmail.com>
2024-05-08 20:25:22 -0300

    Update Hotel.java

--- SistemaHotel/src/Hotel.java
+++ SistemaHotel/src/Hotel.java
@@ -6,11 +6,14 @@ public class Hotel {
     private List<Hospede> hospedes;
     private List<Quarto> quartos;
     private Hospede hospede;
+    private List<Grupo> grupos;
+
 
     public Hotel(List<Hospede> hospedes) {
         this.recepcionistas = new ArrayList<>();
         this.hospedes = new ArrayList<>(hospedes);
         this.quartos = new ArrayList<>();
+        this.grupos = new ArrayList<>();
 
     }
 
