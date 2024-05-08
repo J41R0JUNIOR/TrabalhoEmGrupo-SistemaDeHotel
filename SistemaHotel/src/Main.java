d9f4fe5fe37b41e53b1e1b0c4cbcce9c2f924ace
Guilherme Akio <guiakio8@gmail.com>
2024-05-08 20:28:36 -0300

    Criando lógica de separar em grupos

--- SistemaHotel/src/Main.java
+++ SistemaHotel/src/Main.java
@@ -15,6 +15,40 @@ public class Main {
 
     }
 
+    public static List<Grupo> criarGrupos(List<Hospede> hospedes){
+        // Criar grupos de hóspedes aleatoriamente
+        ArrayList<GrupoHospedes> grupos = new ArrayList<>();
+        Random rand = new Random();
+
+        int indexGroupId = 1;
+        while (!hospedes.isEmpty()) {
+            ArrayList<Hospede> hospedesGrupo = new ArrayList<>();
+
+            // Gera uma quantidade aleatória de hóspedes para o grupo atual
+            if (hospedes.size() > 0) {
+                int tamanhoGrupo = rand.nextInt(hospedes.size()) + 1;
+
+                if (tamanhoGrupo > 4) {
+                    tamanhoGrupo = 4;
+                }
+
+                // Adiciona hóspedes ao grupo
+                for (int i = 0; i < tamanhoGrupo; i++) {
+                    hospedes.get(0).groupId = indexGroupId;
+                    hospedesGrupo.add(hospedes.remove(0)); // Remove e adiciona o primeiro hóspede da lista
+                }
+
+                GrupoHospedes novoGrupo = new GrupoHospedes(hospedesGrupo, indexGroupId);
+
+                grupos.add(novoGrupo);
+
+                // Index geral do grupo
+                indexGroupId += 1;
+            }
+        }
+        return null;
+    }
+
     public static List<Hospede> criarHospedes(int quantidade) {
         List<Hospede> criandoHospedes = new ArrayList<>();
         for (int i = 1; i <= quantidade; i++) {
